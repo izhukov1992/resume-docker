3782ed8c68a3a84e9010ad781cf75e9bd9ddcd64
Ilya Zhukov <izhukov1992@gmail.com>
2018-02-06 10:48:38 +0300

    Use env variables for Amazon S3

--- docker/django/django-entrypoint.sh
+++ docker/django/django-entrypoint.sh
@@ -6,7 +6,6 @@ git reset --hard origin/master
 
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py initsuperuser
 python manage.py collectstatic --noinput
 
 gunicorn --bind unix:resume.sock resume.wsgi
