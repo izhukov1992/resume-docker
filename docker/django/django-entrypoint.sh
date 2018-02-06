16a5772e07cfed14e527335ad931e49716361462
Ilya Zhukov <izhukov1992@gmail.com>
2018-02-06 10:47:56 +0300

    Use Gunicort from requirements

--- docker/django/django-entrypoint.sh
+++ docker/django/django-entrypoint.sh
@@ -5,8 +5,8 @@ git fetch origin
 git reset --hard origin/master
 
 pip install -r requirements.txt
-pip install gunicorn
 python manage.py migrate
+python manage.py initsuperuser
 python manage.py collectstatic --noinput
 
 gunicorn --bind unix:resume.sock resume.wsgi
